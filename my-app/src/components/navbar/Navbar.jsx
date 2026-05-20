57746ac76d752eafeab1e95da72f9f876af09813
qazimoizz <qazimoiz222@gmail.com>
2026-05-20 22:20:57 +0500

    make a function in a function and call it into h1, p, input etc.. tags directly and using arrow function also by parameters and direct

--- my-app/src/components/navbar/Navbar.jsx
+++ my-app/src/components/navbar/Navbar.jsx
@@ -6,18 +6,41 @@ function Navbar() {
         fontFamily : "sans-serif",
         textAlign : "center"
     }
-    const name ="qazi moiz"
-
     const myfunc  = (yourname) =>{
         return   yourname; 
     }
+    const someMessage = ()=>{
+        alert("button was clicked")
+    }    
+    
+    const name ="qazi moiz";
 
+    const age =18;
 
+    const handleOver = () => console.log("mouse is over the text!");
+
+    const handleDoubleClick = () => console.log("mouse is doubleClicked !");
+
+    const handleInput = (event)=> {
+        console.clear();
+        
+        console.log("value :", event.target.value );
+        
+    }
     return (
     <>
       <h1 style={navbarStyle}>Navbar</h1>
         <h2>{myfunc(name)}</h2>
+        <h3>{myfunc(age)}</h3>
 
+        <p onMouseOver={handleOver} onDoubleClick={handleDoubleClick} > Lorem ipsum dolor sit amet.</p>
+
+        <button onClick={someMessage}>CLick me </button>
+        <button onClick={ ()=>{ alert("hell yeahhh")} }> just click me</button>
+        <br />
+        <input type="text" onChange={handleInput} placeholder="write something" />
+    
+    
     </>
   );
 }
