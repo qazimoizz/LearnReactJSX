d651b598e28cfe39597940c1b58099baf8120de6
qazimoizz <qazimoiz222@gmail.com>
2026-05-23 22:18:56 +0500

    added one more

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,31 +1,45 @@
 import Practice from "./components/practiceone/Practice";
+import Button from "./components/button/Button";
 // import PraticetTwo from "./components/practicetwo/PraticetTwo";
 import "./App.css";
+import { useState } from "react";
 function App() {
   const hobbies = ["traveling", "Reading", "Gaming"];
-  const hobbiesTwo =["traveling", "Reading", "Gaming", "cooking", "foodie", "Switzerland"];
+  const hobbiesTwo = [
+    "traveling",
+    "Reading",
+    "Gaming",
+    "cooking",
+    "foodie",
+    "Switzerland",
+  ];
+  // function handleClick() {
+  //   return alert("button is clicked");
+  // }
+
+  const [counter, setCount] = useState(0);
 
   return (
-  <>
-  <Practice name="qazi moiz" age="21" city="Karachi" hobbies={hobbies}/>
-  <Practice name="yumna" age="18" city="Karachi" hobbies={hobbies}/>
-  <Practice name="ayra" age="4" city="Karachi" hobbies={hobbies}/>
-  <Practice name="zulaykha" age="4" city="Karachi" hobbies={hobbies}/>
-  <Practice name="Fred" age="4" city="Karachi" hobbies={hobbies}/>
-  <Practice name="ellie" age="4" city="Karachi" hobbies={hobbies}/>
-  <Practice name="Fredrickson and Ellie" age="4" city="Karachi" hobbies={hobbiesTwo}/>
-  <Practice name="Fredrickson and Ellie" age="4" city="Karachi" hobbies={hobbiesTwo}/>
-  <Practice name="Fredrickson and Ellie" age="4" city="Karachi" hobbies={hobbiesTwo}/>
-  <Practice name="Fredrickson and Ellie" age="4" city="Karachi" hobbies={hobbiesTwo}/>
-  <Practice name="Fredrickson and Ellie" age="4" city="Karachi" hobbies={hobbiesTwo}/>
-  <Practice name="Fredrickson and Ellie" age="4" city="Karachi" hobbies={hobbiesTwo}/>
-  <Practice name="Fredrickson and Ellie" age="4" city="Karachi" hobbies={hobbiesTwo}/>
-  </>
-)
+    <>
+      <Practice name="qazi moiz" age="21" city="Karachi" hobbies={hobbies} />
+      <Practice name="yumna" age="18" city="Karachi" hobbies={hobbies} />
+      <Practice name="ayra" age="4" city="Karachi" hobbies={hobbies} />
+      <Practice name="zulaykha" age="4" city="Karachi" hobbies={hobbies} />
+      <Practice name="Fred" age="4" city="Karachi" hobbies={hobbies} />
+      <Practice name="ellie" age="4" city="Karachi" hobbies={hobbies} />
+      <Practice
+        name="Fredrickson and Ellie"
+        age="4"
+        city="Karachi"
+        hobbies={hobbiesTwo}
+      />
+      {/* <Button label="Click Me" handleClick={handleClick} /> */}
+      <Button onCLick={()=> setCount(counter + 1)} counter={counter}/>
+    </>
+  );
 }
-// import reactLogo from './assets/react.svg'
 // import Navbar from "./components/navbar/Navbar";
-// import Button from "./components/button/Button";
+// import reactLogo from './assets/react.svg'
 /* <Button />
 <br />
 <Navbar /> */
--- my-app/src/components/button/Button.jsx
+++ my-app/src/components/button/Button.jsx
@@ -1,23 +1,32 @@
 import "./Button.css";
 
-function Button() {
-  // let fruits = ["apple", "mango", "banana", "leechy"];
+function Button({onClick,counter}) {
 
-  // return (
-  //   <div>
-  //     <h1> Fruits </h1>
-
-  //     <ul>
-  //       {fruits.map((fruit, index) => (
-  //         <li key={index}>
-  //           {" "}
-  //           {index} - {fruit}
-  //         </li>
-  //       ))}
-  //     </ul>
-  //   </div>
-  // );
+ return (
+  <>
+ {/* <button onClick={change}>{label}</button> */}
+ <h2>counter: {counter}</h2>
+ <button onClick={onClick}>Click the button</button>
+  </>
+ 
+)
 }
+// let fruits = ["apple", "mango", "banana", "leechy"];
+
+// return (
+//   <div>
+//     <h1> Fruits </h1>
+
+//     <ul>
+//       {fruits.map((fruit, index) => (
+//         <li key={index}>
+//           {" "}
+//           {index} - {fruit}
+//         </li>
+//       ))}
+//     </ul>
+//   </div>
+// );
 //! first day code of Learning ReactJS
 // const fullName = "Qazi moiz ur Rehman";
 // return (
